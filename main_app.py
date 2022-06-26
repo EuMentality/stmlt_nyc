88bed72296c369aeb64b846506104c808fa97b66
eu_mental <val.mitkv@gmail.com>
2022-06-26 20:28:26 +0300

    add src & cfg

--- main_app.py
+++ main_app.py
@@ -1,24 +1,24 @@
-import requests   
 import pytz
-import folium 
 import streamlit as st
-import pandas as pd
 import numpy as np
 import osmnx as ox
-import networkx as nx
 from datetime import datetime
 from geopy.geocoders import Nominatim
 from streamlit_folium import folium_static
+from src import find_shortest_route, predict_trip_duration, upload_config
 
+
+# 1. Upload Config
+cfg = upload_config('config/params.yaml')
+url_backend = cfg['backend']['url']
+map_with = cfg['map']['width']
+map_height = cfg['map']['height']
 
 # 1. Sidebar Settings
-
-## 1.1: Trip Config
 st.sidebar.header('Information About The Trip')
-st.sidebar.subheader('Follow this link:  [Google Maps](https://www.google.com/maps/@40.7508729,-73.9704699,13.46z)')
+st.sidebar.subheader('Follow this link:  [Google Maps](https://www.google.com/maps/place/Manhattan,+New+York,+NY,+USA/@40.7604865,-73.9767845,14342m/data=!3m1!1e3!4m5!3m4!1s0x89c2588f046ee661:0xa0b3281fcecc08c!8m2!3d40.7830603!4d-73.9712488)')
 page_names = ['Enter Address', 'Enter Coordinates']
 page = st.sidebar.radio('Choose The Navigation Type', page_names)
-
 if page == 'Enter Coordinates':
     pickup_latitude = st.sidebar.number_input('Pickup Latitude', 40.700000, 40.840010, 40.765990, step=0.00001, format="%.6f")
     pickup_longitude = st.sidebar.number_input('Pickup Longitude', -74.020000, -73.930100, -73.980000, step=0.00001, format="%.6f")
@@ -45,59 +45,24 @@ else:
     vendor_id = int(np.random.choice([1, 2], 1)[0])
     store_and_fwd_flag = str(np.random.choice(['N', 'Y'], 1)[0])
 
-data = {'pickup_latitude': pickup_latitude,
-        'pickup_longitude': pickup_longitude, 
-        'dropoff_latitude': dropoff_latitude,
-        'dropoff_longitude': dropoff_longitude,
-        'pickup_datetime': pickup_datetime,
-        'passenger_count': passenger_count,
-        'vendor_id': vendor_id,
-        'store_and_fwd_flag': store_and_fwd_flag}
-
-## 1.2: Predicting Trip Duration
-def predict(url, json_obj):
-    resp = requests.post(f'{url}',
-                    json=json_obj)
-    return resp.json()
-
-
-url_backend = 'https://taxi-nyc-fastapi.herokuapp.com/predict'
-if st.sidebar.button('Predict Trip Duration!'):
-    ans = predict(url_backend, data)
-    ans_text = f'The duration of your trip will be {int(ans["prediction"])} minutes!'
-    title = f'<p style="font-family:sans-serif; color:Black; font-size: 14px;">{ans_text}</p>'
-    st.sidebar.write(title, unsafe_allow_html=True)
-
+trip_params = {'pickup_latitude': pickup_latitude,
+                'pickup_longitude': pickup_longitude, 
+                'dropoff_latitude': dropoff_latitude,
+                'dropoff_longitude': dropoff_longitude,
+                'pickup_datetime': pickup_datetime,
+                'passenger_count': passenger_count,
+                'vendor_id': vendor_id,
+                'store_and_fwd_flag': store_and_fwd_flag}
+start_trip_coords = (pickup_latitude, pickup_longitude)
+end_trip_coords = (dropoff_latitude, dropoff_longitude)
 
 # 2. Main Page Seggings
-st.write(' # Manhattan Taxi Trip Duration')
+st.write(' # Manhattan Taxi Trip')
 ox.config(log_console=True, use_cache=True)
-
-## 2.1
-def find_shortest_route():
-    start_latlng = (pickup_latitude, pickup_longitude)
-    end_latlng = (dropoff_latitude, dropoff_longitude)
-    place = 'Manhattan, New York, United States'
-    mode = 'drive'        
-    optimizer = 'length' 
-    graph = ox.graph_from_place(place, network_type = mode)
-    orig_node = ox.get_nearest_node(graph, start_latlng)
-    dest_node = ox.get_nearest_node(graph, end_latlng)
-    shortest_route = nx.shortest_path(graph, source=orig_node, target=dest_node, weight=optimizer)
-    shortest_route_map = ox.plot_route_folium(graph, shortest_route, tiles='openstreetmap', route_color='#6495ED')
-    start_latlng = (start_latlng[0],start_latlng[1])
-    end_latlng   = (end_latlng[0],end_latlng[1])
-    start_marker = folium.Marker(location = start_latlng, icon = folium.Icon(color='green'))
-    end_marker = folium.Marker(location = end_latlng, icon = folium.Icon(color='red'))
-    start_marker.add_to(shortest_route_map)
-    end_marker.add_to(shortest_route_map)
-    return shortest_route_map
-
-
-page_names = ["I'm not interested", 'Show The Route of the Trip on The Map!']
-page = st.radio('You know what to choose!', page_names)
-if page ==  'Show The Route of the Trip on The Map!':
-    with st.spinner('Please wait 15 seconds, calculating the optimal route is resource-intensive ^_^'):
-        shortest_route = find_shortest_route()
-        st_data = folium_static(shortest_route, width=700)
-    st.success('Done!')
+if st.sidebar.button('Predict Trip Duration!'):
+    ans = predict_trip_duration(url_backend, trip_params)
+    ans_text = f'The duration of your trip will be {int(ans["prediction"])} minutes!'
+    with st.spinner('The route of the trip is being built ^_^'):
+            shortest_route = find_shortest_route(start_trip_coords, end_trip_coords)
+            st_data = folium_static(shortest_route, width=map_with, height=map_height)
+    st.success(ans_text)
